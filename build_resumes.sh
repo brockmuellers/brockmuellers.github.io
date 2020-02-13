9eab691e95641b4971dd032034b43be15d4a6aa8
sara <sara.brockmueller@gmail.com>
2020-02-13 12:29:51 -0800

    fix broken resume pdf

--- build_resumes.sh
+++ build_resumes.sh
@@ -2,8 +2,6 @@
 
 RESUME_DIR=`dirname "$0"`/assets/resume
 
-cd $RESUME_DIR
-
-lualatex -synctex=1 -interaction=nonstopmode "resume_simple".tex
-
+cd $RESUME_DIR &&
+lualatex -synctex=1 -interaction=nonstopmode "resume_simple".tex &&
 lualatex -synctex=1 -interaction=nonstopmode "resume_large".tex

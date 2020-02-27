c8907ee5d53e11b303187541efd2958303ce2559
sara <sara@3scan.com>
2020-02-26 22:51:38 -0800

    yaml resume file is easier to edit

--- build_resumes.sh
+++ build_resumes.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 RESUME_DIR=`dirname "$0"`/assets/resume
+JSON_OUT_FILE=resume.json
+JSON_PATH=${RESUME_DIR}/${JSON_OUT_FILE}
+
+# Translate pretty YAML file into ugly JSON file so lua can handle it
+cat _data/resume.yml | python3 -c 'import sys, yaml, json; y=yaml.load(sys.stdin.read()); print(json.dumps(y))' > $JSON_PATH &&
 
 cd $RESUME_DIR &&
 lualatex -synctex=1 -interaction=nonstopmode "resume_simple".tex &&
-lualatex -synctex=1 -interaction=nonstopmode "resume_large".tex
+lualatex -synctex=1 -interaction=nonstopmode "resume_large".tex &&
+rm $JSON_OUT_FILE

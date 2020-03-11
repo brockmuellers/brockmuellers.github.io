b6a73eb4ad0cdfff12a575ebe7a32196123aefd4
sara <sara.brockmueller@gmail.com>
2020-03-10 17:14:51 -0700

    resume backup and docs about packages

--- build_resumes.sh
+++ build_resumes.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DATE_FOR_BACKUP=$1 # e.g. 2020_03_31
+
+BACKUP_DIR=~/Dropbox/Documents/Work/Resumes
 RESUME_DIR=`dirname "$0"`/assets/resume
 JSON_OUT_FILE=resume.json
 JSON_PATH=${RESUME_DIR}/${JSON_OUT_FILE}
@@ -10,4 +13,12 @@ cat _data/resume.yml | python3 -c 'import sys, yaml, json; y=yaml.load(sys.stdin
 cd $RESUME_DIR &&
 lualatex -synctex=1 -interaction=nonstopmode "resume_simple".tex &&
 lualatex -synctex=1 -interaction=nonstopmode "resume_large".tex &&
-rm $JSON_OUT_FILE
+echo "Successfully generated resumes." &&
+
+if [ "$DATE_FOR_BACKUP" != "" ]
+then
+    BACKUP_DEST=${BACKUP_DIR}/${DATE_FOR_BACKUP} &&
+    mkdir $BACKUP_DEST &&
+    cp ./{resume_large.tex,resume_simple.tex,resume_large.pdf,resume_simple.pdf,resume.json} $BACKUP_DEST/ &&
+    echo "Copied files to ${BACKUP_DEST}."
+fi

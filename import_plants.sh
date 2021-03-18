d508023b0dfdd13d63146bd4bfc71eef6e482f44
Sara Brockmueller <sara.brockmueller@gmail.com>
2021-03-18 11:47:11 -0700

    Support importing google sheets plant files

---
+++ import_plants.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Designed for a csv file exported by google sheets, which may have carriage returns.
+
+INFILE=$1 # e.g. 2020_03_31
+
+OUTFILE=`dirname "$0"`/_data/plants.csv
+
+cp "$INFILE" $OUTFILE &&
+echo "Imported ${INFILE}" &&
+
+dos2unix $OUTFILE &&
+echo "Removed carriage returns"
+
+
